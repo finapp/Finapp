5ecde2b2ebcb70431593191e9fbbaac5e4b03bac
finapp <dev1@bizpark.co>
2017-11-03 10:00:37 +0100

    Added IAlgorithms interface

--- Finapp/Interfaces/IAlgorithms.cs
+++ Finapp/Interfaces/IAlgorithms.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Finapp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Finapp.Interfaces
 {
-    interface IAlgorithms
+    public interface IAlgorithms
     {
+        Queue<Debtor> addDebtorsToQueue();
+        Queue<Creditor> addCreditorsToQueue();
+        bool associating();
+
     }
 }
